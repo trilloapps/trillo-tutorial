081038a14a5ce4e7c2f037d96a78a8426bbfc1c6
Anil Sharma <anil@trillo.io>
2023-12-24 23:52:56 -0800

    Added DataIteratorLesson function.

---
+++ src/main/java/DataIteratorLesson.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Map;
+import com.collager.trillo.model.DataIterator;
+import com.collager.trillo.pojo.Result;
+import com.collager.trillo.util.Api;
+import com.collager.trillo.util.DSApi;
+import com.collager.trillo.util.LogApi;
+import com.collager.trillo.util.ServerlessFunction;
+
+public class DataIteratorLesson extends ServerlessFunction {
+  
+  private String SQL_QUERY_TEMPLATE = "SELECT " + 
+      "  c.id AS customer_id, " + 
+      "  c.firstName AS first_name, " + 
+      "  c.lastName AS last_name, " + 
+      "  li.id AS line_item_id, " + 
+      "  li.description, " + 
+      "  li.price " + 
+      "FROM " + 
+      "  Customer_tbl c " + 
+      "JOIN " + 
+      "  LineItem_tbl li ON c.id = li.customerId " + 
+      "WHERE " + 
+      "  c.id = {{{id}}}";
+  
+  @Api(httpMethod = "get")
+  public Object iterate(Map<String, Object> parameters) {
+    DataIterator dataIterator = DSApi.getDataIterator(1, 3, SQL_QUERY_TEMPLATE);
+    dataIterator.getDataRequest().setParams(parameters);
+    Result r = dataIterator.initialize();
+
+    if (r.isFailed()) {
+      LogApi.error("Failed to initialize DataIterator", "error", r.getMessage());
+      return r;
+    }
+
+    Integer totalCount = dataIterator.getTotalItems();
+
+    LogApi.auditLogInfo("Total number of items : " + totalCount);
+    ArrayList<Map<String, Object>> allItems = new ArrayList<>();
+
+    while (dataIterator.hasNext()) {
+      ArrayList<Map<String, Object>> items = (ArrayList<Map<String, Object>>) dataIterator.getPage();
+      allItems.addAll(items);
+    }
+    return allItems;
+  }
+}
