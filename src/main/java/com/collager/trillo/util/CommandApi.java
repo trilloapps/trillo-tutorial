4ade56f1b36c1c6a2568681a26889f5d7df2848d
trishla <trishla@trillo.io>
2024-02-18 17:14:00 +0530

    added command api fix

--- src/main/java/com/collager/trillo/util/CommandApi.java
+++ src/main/java/com/collager/trillo/util/CommandApi.java
@@ -14,7 +14,7 @@ public class CommandApi extends BaseApi {
   private static String commandEndpoint = "/api/v1.1/command";
   public static Result runOSCmd(List<String> argList) {
     Map body = new HashMap();
-    body.put("argList1", argList);
+    body.put("argList", argList);
     Object obj = HttpRequestUtil.post(commandEndpoint + "/runOSCmd", new JSONObject(body).toString());
     return convertToResult(obj);
   }
